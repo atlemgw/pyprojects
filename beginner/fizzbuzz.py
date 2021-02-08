303214d5c7e1c943b34e655b17879b36b1747994
Atle M. G. Winther <31645802+atlemgw@users.noreply.github.com>
2021-02-08 21:10:53 +0100

    Add files via upload

---
+++ beginner/fizzbuzz.py
@@ -0,0 +1,24 @@
+# Fizz Buzz
+
+def fizzbuzz(z):
+    # Loop
+    forn in range(1, n + 1):
+        # Fizz Buzz
+        if tal % 3 == 0 and tal % 5 == 0:
+            print("FizzBuzz")
+            
+        # Fizz
+        elif tal % 3 == 0:
+            print("Fizz")
+            
+        # Buzz
+        elif tal % 5 == 0:
+            print("Buzz")
+
+        # Number
+        else:
+            print(tal)
+
+fizzbuzz(100)
+        
+        
