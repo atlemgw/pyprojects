4653fad7a64d6184f0a39eafcaf4ae47c4b48d93
Atle M. G. Winther <31645802+atlemgw@users.noreply.github.com>
2021-02-08 21:12:02 +0100

    Update fizzbuzz.py

--- beginner/fizzbuzz.py
+++ beginner/fizzbuzz.py
@@ -2,22 +2,22 @@
 
 def fizzbuzz(z):
     # Loop
-    forn in range(1, n + 1):
+    for n in range(1, z + 1):
         # Fizz Buzz
-        if tal % 3 == 0 and tal % 5 == 0:
+        if n % 3 == 0 and n % 5 == 0:
             print("FizzBuzz")
             
         # Fizz
-        elif tal % 3 == 0:
+        elif n % 3 == 0:
             print("Fizz")
             
         # Buzz
-        elif tal % 5 == 0:
+        elif n % 5 == 0:
             print("Buzz")
 
         # Number
         else:
-            print(tal)
+            print(n)
 
 fizzbuzz(100)
         
