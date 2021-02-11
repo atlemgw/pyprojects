f6d4a52fecd6c4ad1d6e9daa2aef0b1c36a03ea9
Atle M. G. Winther <31645802+atlemgw@users.noreply.github.com>
2021-02-11 11:29:41 +0100

    Update fizzbuzz.py

--- beginner/fizzbuzz.py
+++ beginner/fizzbuzz.py
@@ -1,5 +1,6 @@
 # Fizz Buzz
 
+# Version 1 as a function
 def fizzbuzz(z):
     # Loop
     for n in range(1, z + 1):
@@ -21,4 +22,21 @@ def fizzbuzz(z):
 
 fizzbuzz(100)
         
-        
+# Version 2        
+print("Version 2")
+for tal in range(1, 101):
+    fizzbuzz = ''
+
+    if tal % 3 == 0:
+        fizzbuzz = "Fizz"
+
+    if tal % 5 == 0:
+        fizzbuzz += "Buzz"
+
+    print(fizzbuzz or tal)
+
+# Version 3
+print()
+print("Version very simple!!")
+for tal in range(1, 101):
+    print("Fizz" * (tal % 3 == 0) + (tal % 5 == 0) * "Buzz" or tal)
